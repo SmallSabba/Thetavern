9ad307496ab6154abe31d4e4fd9692aec0ab2bf4
KilianHnidek <kilian.hnidek@stud.fh-campuswien.ac.at>
2022-05-21 21:34:56 +0200

    bla bla bla bla bla bla

---
+++ src/main/web/scripting/index.js
@@ -0,0 +1,39 @@
+window.onload = function () {
+    const backgroundImg =
+        [
+            "https://images.unsplash.com/photo-1501785888041-af3ef285b470?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80",
+            "https://images.unsplash.com/photo-1517057011470-8f36d636e6ca?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80",
+            "https://images.unsplash.com/flagged/photo-1552035791-b3cc1632e933?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2069&q=80",
+            "https://images.unsplash.com/photo-1574700273608-7962d3602a9f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80",
+            "https://images.unsplash.com/photo-1605045544284-d13c6d6a60a6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
+        ];
+
+    const headerHeading =
+        [
+            "Travel the World",
+            "No Restrictions",
+            "Enjoy life to the fullest"
+        ];
+
+    const headerSubheading =
+        [
+            "There are no obstacles anymore",
+            "Roam like you want",
+            "That's your company's motto"
+        ];
+
+    function changeImage() {
+        const i = Math.floor((Math.random() * 3));
+
+        document.getElementById("containerHeader").style.backgroundImage = "url('" + backgroundImg[i] + "')";
+        document.getElementById("headerHeading").innerText = headerHeading[i];
+        document.getElementById("headerSubheading").innerText = headerSubheading[i];
+    }
+
+    setInterval(changeImage, 5000);
+}
+
+
+
+
+
