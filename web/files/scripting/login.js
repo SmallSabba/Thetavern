6ba3987b646d452ab0ac854bfc76a241eb7887ac
KilianHnidek <kilian.hnidek@stud.fh-campuswien.ac.at>
2022-06-18 00:41:03 +0200

    Namensänderung

--- web/files/scripting/login.js
+++ web/files/scripting/login.js
@@ -1,6 +1,17 @@
 const resetButton = document.querySelector('.resetButton');
+const submitButton = document.querySelector('.submitButton');
 const loginForm = document.querySelector('.loginForm');
+const errorMsg = document.querySelector('.errorMsg');
+
 
 resetButton.addEventListener('click', () => {
     loginForm.reset();
-})
+});
+
+submitButton.addEventListener('click', () => {
+
+});
+
+
+
+
